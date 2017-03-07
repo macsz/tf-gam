5036fc640ff7d6ae0156550f5fde4ed8c90ff09b
Maciej Szankin <maciej@szankin.pl>
2017-03-06 23:34:33 -0600

    Performance fix #3

--- class/classify_face.py
+++ class/classify_face.py
@@ -44,8 +44,7 @@ class ClassifyFace:
             graph_def.ParseFromString(f.read())
             _ = tf.import_graph_def(graph_def, name='')
 
-    def _run_tf(self, image_data):
-        with tf.Session() as sess:
+    def _run_tf(self, sess, image_data):
             # Feed the image_data as input to the graph and get first prediction
 
             softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
@@ -78,81 +77,82 @@ class ClassifyFace:
     def run(self):
         self._load_tf()
         counter = 0
+        with tf.Session() as sess:
 
-        for image_path in self._files:
-            counter += 1
+            for image_path in self._files:
+                counter += 1
 
-            # Read in the image_data
-            image_data = tf.gfile.FastGFile(image_path, 'rb').read()
+                # Read in the image_data
+                image_data = tf.gfile.FastGFile(image_path, 'rb').read()
 
-            time_start = timer()
-            self._run_tf(image_data)
-            time_elapsed = timer() - time_start
-            print('{0}/{1}'.format(counter, len(self._files)), 'Face TF elapsed time', image_path, time_elapsed)
+                time_start = timer()
+                self._run_tf(sess, image_data)
+                time_elapsed = timer() - time_start
+                print('{0}/{1}'.format(counter, len(self._files)), 'Face TF elapsed time', image_path, time_elapsed)
 
-            img_full = np.array(Image.open(image_path), dtype=np.uint8)
+                img_full = np.array(Image.open(image_path), dtype=np.uint8)
 
-            # Create figure and axes
-            fig, ax = plt.subplots(1)
+                # Create figure and axes
+                fig, ax = plt.subplots(1)
 
-            # Display the image
-            ax.imshow(img_full)
+                # Display the image
+                ax.imshow(img_full)
 
-            max_h = -1
-            max_r = -1
-            min_h = 999
-            min_r = 999
-            for h in range(0, 8):
-                for r in range(0, 8):
-                    prob = self._mat[str(r) + ',' + str(h)]
-                    if prob > THRESHOLD_DOWN:
-                        tile = patches.Rectangle((80 / 8 * h, 60 / 8 * r), 80 / 8, 60 / 8, linewidth=1, edgecolor='g',
-                                                 facecolor='g', alpha=self.get_color_intensity(prob, norm=False))
-                        if h < min_h:
-                            min_h = h
-                        if r < min_r:
-                            min_r = r
-                        if h > max_h:
-                            max_h = h
-                        if r > max_r:
-                            max_r = r
-                    else:
-                        tile = patches.Rectangle((80/8 * h, 60/8 * r), 80/8, 60/8, linewidth=1, edgecolor='r', facecolor='none',
-                                                 alpha=0.5)
-                    ax.add_patch(tile)
+                max_h = -1
+                max_r = -1
+                min_h = 999
+                min_r = 999
+                for h in range(0, 8):
+                    for r in range(0, 8):
+                        prob = self._mat[str(r) + ',' + str(h)]
+                        if prob > THRESHOLD_DOWN:
+                            tile = patches.Rectangle((80 / 8 * h, 60 / 8 * r), 80 / 8, 60 / 8, linewidth=1, edgecolor='g',
+                                                     facecolor='g', alpha=self.get_color_intensity(prob, norm=False))
+                            if h < min_h:
+                                min_h = h
+                            if r < min_r:
+                                min_r = r
+                            if h > max_h:
+                                max_h = h
+                            if r > max_r:
+                                max_r = r
+                        else:
+                            tile = patches.Rectangle((80/8 * h, 60/8 * r), 80/8, 60/8, linewidth=1, edgecolor='r', facecolor='none',
+                                                     alpha=0.5)
+                        ax.add_patch(tile)
 
-            detected_face = patches.Rectangle((80 / 8 * min_h, 60 / 8 * min_r), 80 / 8 * (max_h-min_h+1),
-                                              60 / 8 * (max_r-min_r+1), linewidth=3, edgecolor='g', facecolor='none')
-            ax.add_patch(detected_face)
+                detected_face = patches.Rectangle((80 / 8 * min_h, 60 / 8 * min_r), 80 / 8 * (max_h-min_h+1),
+                                                  60 / 8 * (max_r-min_r+1), linewidth=3, edgecolor='g', facecolor='none')
+                ax.add_patch(detected_face)
 
-            img_face = img_full[min_r*int(60 / 8):(max_r+1)*int(60 / 8), min_h*int(80 / 8):(max_h+1)*int(80 / 8)]
+                img_face = img_full[min_r*int(60 / 8):(max_r+1)*int(60 / 8), min_h*int(80 / 8):(max_h+1)*int(80 / 8)]
 
-            # plt.show()
-            save_path = image_path.split('/')
-            save_path[-2] = 'output_face'
-            save_path = '/'.join(save_path)
-            plt.savefig(save_path)
+                # plt.show()
+                save_path = image_path.split('/')
+                save_path[-2] = 'output_face'
+                save_path = '/'.join(save_path)
+                plt.savefig(save_path)
 
-            nose_path = image_path.split('/')
-            nose_path[-2] = 'input_nose'
-            nose_path = '/'.join(nose_path)
-            scipy.misc.imsave(nose_path, img_face)
+                nose_path = image_path.split('/')
+                nose_path[-2] = 'input_nose'
+                nose_path = '/'.join(nose_path)
+                scipy.misc.imsave(nose_path, img_face)
 
-            nose_data = {
-                'orig_path': image_path,
-                'nose_path': nose_path,
-                'nose_position': {
-                    'min_h': min_h,
-                    'min_r': min_r,
-                    'max_h': max_h,
-                    'max_r': max_r
-                },
-                'data': img_face
-            }
-            self.noses[image_path] = nose_data
-            plt.clf()
-            plt.cla()
-            plt.close('all')
+                nose_data = {
+                    'orig_path': image_path,
+                    'nose_path': nose_path,
+                    'nose_position': {
+                        'min_h': min_h,
+                        'min_r': min_r,
+                        'max_h': max_h,
+                        'max_r': max_r
+                    },
+                    'data': img_face
+                }
+                self.noses[image_path] = nose_data
+                plt.clf()
+                plt.cla()
+                plt.close('all')
 
     def __init__(self, files, model_path):
         self._model_path = model_path
--- class/classify_nose.py
+++ class/classify_nose.py
@@ -44,88 +44,87 @@ class ClassifyNose:
             graph_def.ParseFromString(f.read())
             _ = tf.import_graph_def(graph_def, name='')
 
-    def _run_tf(self, image_data):
-        with tf.Session() as sess:
-            # Feed the image_data as input to the graph and get first prediction
-            init = tf.global_variables_initializer()
-            sess.run(init)
+    def _run_tf(self, sess, image_data):
+        # Feed the image_data as input to the graph and get first prediction
+        init = tf.global_variables_initializer()
+        sess.run(init)
 
-            softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
-            feature_tensor = sess.graph.get_tensor_by_name('mixed_10/join:0')
-            # feature_set: 1x 8x8x2048
-            feature_set = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
+        softmax_tensor = sess.graph.get_tensor_by_name('final_result:0')
+        feature_tensor = sess.graph.get_tensor_by_name('mixed_10/join:0')
+        # feature_set: 1x 8x8x2048
+        feature_set = sess.run(feature_tensor, {'DecodeJpeg/contents:0': image_data})
 
-            for cells in feature_set:
-                for x in range(0, len(cells)):
-                    for y in range(0, len(cells[x])):
-                        cell = cells[x][y]
-                        cell = np.reshape(cell, (1,1,1,2048))
-                        a = sess.run(softmax_tensor, {'pool_3:0': cell})
+        for cells in feature_set:
+            for x in range(0, len(cells)):
+                for y in range(0, len(cells[x])):
+                    cell = cells[x][y]
+                    cell = np.reshape(cell, (1, 1, 1,2048))
+                    a = sess.run(softmax_tensor, {'pool_3:0': cell})
 
-                        # res = tf.nn.softmax(a)
-                        #
-                        # res = sess.run(res)
-                        probabilities = a[0]
-                        top_k = probabilities.argsort()[-len(probabilities):][::-1]
-                        for node_id in top_k:
-                            human_string = self._label_lines[node_id]
-                            score = probabilities[node_id]
-                            if human_string != 'other':
-                                self._mat[str(x) + ',' + str(y)] = score
-            self._sorted_probs = sorted(self._mat.items(), key=operator.itemgetter(1))
+                    # res = tf.nn.softmax(a)
+                    #
+                    # res = sess.run(res)
+                    probabilities = a[0]
+                    top_k = probabilities.argsort()[-len(probabilities):][::-1]
+                    for node_id in top_k:
+                        human_string = self._label_lines[node_id]
+                        score = probabilities[node_id]
+                        if human_string != 'other':
+                            self._mat[str(x) + ',' + str(y)] = score
+        self._sorted_probs = sorted(self._mat.items(), key=operator.itemgetter(1))
 
     def run(self):
         self._load_tf()
         counter = 0
+        with tf.Session() as sess:
+            for image_path in self._noses.keys():
+                counter += 1
+                image_data = tf.gfile.FastGFile(self._noses[image_path]['nose_path'], 'rb').read()
 
-        for image_path in self._noses.keys():
-            counter += 1
-            image_data = tf.gfile.FastGFile(self._noses[image_path]['nose_path'], 'rb').read()
+                time_start = timer()
+                self._run_tf(sess, image_data)
+                time_elapsed = timer() - time_start
+                print('{0}/{1}'.format(counter, len(self._noses)), 'Nose TF elapsed time', image_path, time_elapsed)
 
-            time_start = timer()
-            self._run_tf(image_data)
-            time_elapsed = timer() - time_start
-            print('{0}/{1}'.format(counter, len(self._noses)), 'Nose TF elapsed time', image_path, time_elapsed)
+                img_full = np.array(Image.open(self._noses[image_path]['nose_path']), dtype=np.uint8)
+                lh, lr = len(img_full), len(img_full[0])
 
-            img_full = np.array(Image.open(self._noses[image_path]['nose_path']), dtype=np.uint8)
-            lh, lr = len(img_full), len(img_full[0])
+                # Create figure and axes
+                fig, ax = plt.subplots(1)
 
-            # Create figure and axes
-            fig, ax = plt.subplots(1)
+                # Display the image
+                ax.imshow(img_full)
 
-            # Display the image
-            ax.imshow(img_full)
+                for h in range(0, 8):
+                    for r in range(0, 8):
+                        prob = self._mat[str(r) + ',' + str(h)]
+                        if prob > THRESHOLD_DOWN:
+                            # k, v = self._sorted_probs[-3:]
 
-            for h in range(0, 8):
-                for r in range(0, 8):
-                    prob = self._mat[str(r) + ',' + str(h)]
-                    if prob > THRESHOLD_DOWN:
-                        # k, v = self._sorted_probs[-3:]
-
-                        if prob in [x[1] for x in self._sorted_probs[-3:]]:
-                            tile = patches.Rectangle((lh / 8 * h, lr / 8 * r), lh / 8, lr / 8, linewidth=1,
-                                                     edgecolor='b',
-                                                     facecolor='b', alpha=self.get_color_intensity(prob, norm=False))
+                            if prob in [x[1] for x in self._sorted_probs[-3:]]:
+                                tile = patches.Rectangle((lh / 8 * h, lr / 8 * r), lh / 8, lr / 8, linewidth=1,
+                                                         edgecolor='b',
+                                                         facecolor='b', alpha=self.get_color_intensity(prob, norm=False))
+                            else:
+                                tile = patches.Rectangle((lh / 8 * h, lr / 8 * r), lh / 8, lr / 8, linewidth=1,
+                                                         edgecolor='g', facecolor='g',
+                                                         alpha=self.get_color_intensity(prob, norm=False))
                         else:
-                            tile = patches.Rectangle((lh / 8 * h, lr / 8 * r), lh / 8, lr / 8, linewidth=1,
-                                                     edgecolor='g', facecolor='g',
-                                                     alpha=self.get_color_intensity(prob, norm=False))
-                    else:
-                        tile = patches.Rectangle((lh/8 * h, lr/8 * r), lh/8, lr/8, linewidth=1, edgecolor='r',
-                                                 facecolor='none',
-                                                 alpha=0.5)
-                    ax.add_patch(tile)
+                            tile = patches.Rectangle((lh/8 * h, lr/8 * r), lh/8, lr/8, linewidth=1, edgecolor='r',
+                                                     facecolor='none',
+                                                     alpha=0.5)
+                        ax.add_patch(tile)
 
-            # img_face = img_full[min_r*int(60 / 8):(max_r+1)*int(60 / 8), min_h*int(80 / 8):(max_h+1)*int(80 / 8)]
+                # img_face = img_full[min_r*int(60 / 8):(max_r+1)*int(60 / 8), min_h*int(80 / 8):(max_h+1)*int(80 / 8)]
 
-            # plt.show()
-            save_path = image_path.split('/')
-            save_path[-2] = 'output_nose'
-            save_path = '/'.join(save_path)
-            plt.savefig(save_path)
-            plt.clf()
-            plt.cla()
-            plt.close('all')
+                # plt.show()
+                save_path = image_path.split('/')
+                save_path[-2] = 'output_nose'
+                save_path = '/'.join(save_path)
+                plt.savefig(save_path)
+                plt.clf()
+                plt.cla()
+                plt.close('all')
 
     def __init__(self, noses, model_path):
         self._model_path = model_path
